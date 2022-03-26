92fa51357bb3f39d30ac170362dfe16a0f2b3b7a
tunabay <55529452+tunabay@users.noreply.github.com>
2022-03-26 11:39:57 +0900

    First draft.

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Hirotsuna Mizuno. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+/*
+Package tdma implements TDMA (Tridiagonal Matrix Algorithm, aka Thomas algorithm)
+for solving systems of equations.
+*/
+package tdma
